54bde08a6eff3171563551bf65c467834b94ebe7
Eduardo Ataide <eataide@outlook.com>
2025-01-10 23:19:03 -0300

    vai

--- dados.php
+++ dados.php
@@ -1,40 +1,6 @@
 <?php
-$livros = [
-    [
-        "id" => 1,
-        "titulo" => "O Senhor dos Anéis - A Sociedade do Anel",
-        "autor" => "J.R.R. Tolkien",
-        "descricao" => "Descrição do livro 1",
-    ],
-    [
-        "id" => 2,
-        "titulo" => "Harry Potter e a Pedra Filosofal",
-        "autor" => "J.K. Rowling",
-        "descricao" => "Descrição do livro 2",
-    ],
-    [
-        "id" => 3,
-        "titulo" => "O Hobbit - A Batalha dos Cinco Exércitos",
-        "autor" => "J.R.R. Tolkien",
-        "descricao" => "Descrição do livro 3",
-    ],
-    [
-        "id" => 4,
-        "titulo" => "O Senhor dos Anéis - As Duas Torres",
-        "autor" => "J.R.R. Tolkien",
-        "descricao" => "Descrição do livro 4",
-    ],
-    [
-        "id" => 5,
-        "titulo" => "O Senhor dos Anéis - O Retorno do Rei",
-        "autor" => "J.R.R. Tolkien",
-        "descricao" => "Descrição do livro 5",
-    ],
-    [
-        "id" => 6,
-        "titulo" => "O Hobbit - A Desolação de Smaug",
-        "autor" => "J.R.R. Tolkien",
-        "descricao" => "Descrição do livro 6",
-    ],
-    
-];
+
+$db = new PDO('sqlite:database.sqlite');
+$query = $db->query("SELECT * FROM livros");
+
+$livros = $query->fetchAll();
